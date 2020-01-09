b19c561b099c62720fe19d84020fdd3dee4e75b8
Oleg Keri <ezhi99@gmail.com>
2020-01-09 09:44:33 +0100

    add handle_events to mixer api

--- src/mixer.rs
+++ src/mixer.rs
@@ -57,6 +57,9 @@ impl Mixer {
         }
     }
 
+    pub fn handle_events(&self) -> Result<u32> {
+        acheck!(snd_mixer_handle_events(self.0)).map(|x| x as u32)
+    }
 
     pub fn wait(&self, timeout_ms: Option<u32>) -> Result<bool> {
         acheck!(snd_mixer_wait(self.0, timeout_ms.map(|x| x as c_int).unwrap_or(-1))).map(|i| i == 1) }
